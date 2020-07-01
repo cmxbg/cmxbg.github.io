e65963a24db7699dae09790e34b0a5c1c98cef9e
cmxbg <67374062+cmxbg@users.noreply.github.com>
2020-07-01 23:14:03 +0800

    Add files via upload

---
+++ HappyBirthday/scripts/main.js
@@ -0,0 +1,10 @@
+let myImage = document.querySelector('i');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/1.jpg') {
+      myImage.setAttribute('src', 'images/2.jpg');
+    } else {
+      myImage.setAttribute('src', 'images/1.jpg');
+    }
+}
